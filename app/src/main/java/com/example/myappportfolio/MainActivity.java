34ec250a1b8a20e0c0cde996c56a2bced4aa30e8
wenhuacong <958460516@qq.com>
2016-08-06 11:03:16 +0800

    添加了六个按钮以及一个按钮的点击事件

--- app/src/main/java/com/example/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/myappportfolio/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showToastPopularMovie(View view) {
+        Toast.makeText(MainActivity.this, "This button will launch my capstone app!", Toast.LENGTH_SHORT).show();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
